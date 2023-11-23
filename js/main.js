980c7f6c2f4c56c1136d344ce06cdd8f8a2adbbb
Marcelo Bizoso <marce.bizoso9@gmail.com>
2023-11-23 12:54:42 -0300

    primer pre-entrega

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let opciones = ["piedra", "papel", "tijera"];
+
+
+
+function ppt () {
+    let jugar = true;
+
+    while (jugar){
+        let eleccionJugador = prompt("Elige piedra, papel o tijera").toLowerCase();
+
+        if (eleccionJugador === 'esc'){
+            jugar = false;
+            console.log("hasta luego")
+            break;
+        }
+
+        let eleccionBot = opciones[Math.floor(Math.random() * 3)];
+            
+        console.log("Tu elegiste: " + eleccionJugador);
+        console.log("El Bot eligió: " + eleccionBot);
+        
+        if (eleccionJugador === eleccionBot) {
+                console.log("Empate :o");
+            } else if (
+                (eleccionJugador === "piedra" && eleccionBot === "tijera") ||
+                (eleccionJugador === "papel" && eleccionBot === "piedra") ||
+                (eleccionJugador === "tijera" && eleccionBot === "papel")
+            ) {
+                console.log("Ganaste :)");
+            } else {
+                console.log("Te gano un bot :(");
+            }
+    }
+}
+ppt ();
+
+
