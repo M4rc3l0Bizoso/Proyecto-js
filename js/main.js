54319fa75ccf22484bcdf1b3a4d403133f4ee26d
Marcelo Bizoso <marce.bizoso9@gmail.com>
2023-12-12 17:33:56 -0300

    segunda pre entrega

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-let opciones = ["piedra", "papel", "tijera"];
+const opciones = ["piedra", "papel", "tijera"];
 
-function ppt () {
+const opcionRandom = () => opciones [Math.floor(Math.random() * 3)];
+
+const resultados = {
+    empate: "Empate :o",
+    victoria:"Ganaste :)",
+    derrota: "Te gano un bot :("
+};
+
+const jugar = () => {
     let jugar = true;
+
 
     while (jugar){
         let eleccionJugador = prompt("Elige piedra, papel o tijera (si te arrepentiste pone 'salir')").toLowerCase();
@@ -12,24 +21,32 @@ function ppt () {
             break;
         }
 
-        let eleccionBot = opciones[Math.floor(Math.random() * 3)];
-            
+
+        if (opciones.includes(eleccionJugador)){
+            let eleccionBot = opcionRandom();
+
         console.log("Tu elegiste: " + eleccionJugador);
         console.log("El Bot eligió: " + eleccionBot);
         
         if (eleccionJugador === eleccionBot) {
-                console.log("Empate :o");
+                console.log(resultados.empate);
             } else if (
                 (eleccionJugador === "piedra" && eleccionBot === "tijera") ||
                 (eleccionJugador === "papel" && eleccionBot === "piedra") ||
                 (eleccionJugador === "tijera" && eleccionBot === "papel")
             ) {
-                console.log("Ganaste :)");
+                console.log(resultados.victoria);
             } else {
-                console.log("Te gano un bot :(");
+                console.log(resultados.derrota);
+            } 
+        }else{
+                console.log("elige una opcion valida: piedra, papel o tijera");
             }
+        
     }
-}
-ppt ();
+};
+
+
+jugar();
 
 
